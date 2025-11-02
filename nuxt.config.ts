b16f8a056b4568e8d9ae46a1c067663b3e191e5b
Karolis <karoliscd@gmail.com>
2025-11-02 15:51:16 +0200

    Add pre commit hook

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import {defineNuxtConfig} from 'nuxt/config'
-import {fileURLToPath} from 'node:url'
+import { defineNuxtConfig } from 'nuxt/config'
+import { fileURLToPath } from 'node:url'
 
 export default defineNuxtConfig({
-    alias: {
-        "~": fileURLToPath(new URL('./', import.meta.url)),
-    },
-    compatibilityDate: '2025-07-15',
-    devtools: {enabled: true},
-    modules: ['@nuxt/eslint', '@nuxt/image', '@nuxt/ui', '@prisma/nuxt', 'nuxt-file-storage', '@sidebase/nuxt-auth', '@vee-validate/nuxt'],
-    fileStorage: {
-        mount: process.env.MOUNT
-    }
-});
+  alias: {
+    '~': fileURLToPath(new URL('./', import.meta.url)),
+  },
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/image',
+    '@nuxt/ui',
+    '@prisma/nuxt',
+    'nuxt-file-storage',
+    '@vee-validate/nuxt',
+    'nuxt-auth-utils',
+  ],
+  fileStorage: {
+    mount: process.env.MOUNT,
+  },
+})
--- server/api/listing/new.post.ts
+++ server/api/listing/new.post.ts
@@ -1,51 +1,55 @@
-import prisma from "~/lib/prisma";
-import { z } from 'zod';
+import prisma from '~/lib/prisma'
+import { z } from 'zod'
 
 const listingCreateSchema = z.object({
-    title: z.string().min(1, 'Title is required'),
-    authorId: z.string().min(1, 'Author ID is required'),
-    description: z.string().optional(),
-    price: z.number().min(0, "Price is required and must be a positive number"),
-    categoryId: z.string().min(1, 'Category ID is required'),
-});
+  title: z.string().min(1, 'Title is required'),
+  authorId: z.string().min(1, 'Author ID is required'),
+  description: z.string().optional(),
+  price: z.number().min(0, 'Price is required and must be a positive number'),
+  categoryId: z.string().min(1, 'Category ID is required'),
+})
 
 // Create a new listing
 export default defineEventHandler(async (event) => {
-    const body = await readBody(event);
+  const body = await readBody(event)
 
-    const result = listingCreateSchema.safeParse(body);
+  const result = listingCreateSchema.safeParse(body)
 
-    if (!result.success) {
-        throw createError({
-            statusCode: 400,
-            statusMessage: 'Validation Error',
-            data: z.treeifyError(result.error),
-        });
-    }
+  if (!result.success) {
+    throw createError({
+      statusCode: 400,
+      statusMessage: 'Validation Error',
+      data: z.treeifyError(result.error),
+    })
+  }
 
-    // Check if author exists
-    if (!await prisma.user.findUnique({ where: { id: result.data.authorId } })) {
-        throw createError({
-            statusCode: 400,
-            statusMessage: 'Author not found',
-        });
-    }
+  // Check if author exists
+  if (!(await prisma.user.findUnique({ where: { id: result.data.authorId } }))) {
+    throw createError({
+      statusCode: 400,
+      statusMessage: 'Author not found',
+    })
+  }
 
-    // Check if category exists
-    if (!await prisma.category.findUnique({ where: { id: result.data.categoryId } })) {
-        throw createError({
-            statusCode: 400,
-            statusMessage: 'Category not found',
-        });
-    }
+  // Check if category exists
+  if (
+    !(await prisma.category.findUnique({
+      where: { id: result.data.categoryId },
+    }))
+  ) {
+    throw createError({
+      statusCode: 400,
+      statusMessage: 'Category not found',
+    })
+  }
 
-    return prisma.listing.create({
-        data: {
-            title: result.data.title,
-            authorId: result.data.authorId,
-            description: result.data.description,
-            price: result.data.price,
-            categoryId: result.data.categoryId,
-        },
-    });
-});
+  return prisma.listing.create({
+    data: {
+      title: result.data.title,
+      authorId: result.data.authorId,
+      description: result.data.description,
+      price: result.data.price,
+      categoryId: result.data.categoryId,
+    },
+  })
+})
