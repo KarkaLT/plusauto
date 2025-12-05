4ea64499678282b79f3ab65b08572bede64e99c8
Karolis <karoliscd@gmail.com>
2025-12-05 15:55:07 +0200

    feat: add dark mode styling for Swagger UI

--- app/pages/api-docs.vue
+++ app/pages/api-docs.vue
@@ -1,10 +1,17 @@
 <template>
-  <div id="swagger-ui" class="bg-white"/>
+  <div id="swagger-ui" :class="{ 'swagger-dark': isDark }" />
 </template>
 
 <script lang="ts" setup>
 import { SwaggerUIBundle, SwaggerUIStandalonePreset } from 'swagger-ui-dist'
 import 'swagger-ui-dist/swagger-ui.css'
+
+const colorMode = useColorMode()
+const isDark = computed(() => colorMode.value === 'dark')
+
+useHead({
+  link: computed(() => (isDark.value ? [{ rel: 'stylesheet', href: '/SwaggerDark.css' }] : [])),
+})
 
 if (import.meta.client) {
   SwaggerUIBundle({
